2215f9040f5885009657c67c0e6f7a29a8c4944a
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-08-31 11:17:33 -0700

    Update Project 1.py

--- Project 1.py
+++ Project 1.py
@@ -10,8 +10,7 @@ def PseudoGenerator():
     M=8601#Multiplier
 
     S= int(input("Enter a whole number"))
-    term= int (input("Please enter your nth that you want to run"))
-    for i in range(term):
+    for i in range(100):
         S=(M*S+A)%N
         r=S/N#The number r will be in [0,1)
         print(format(r,'.4f'))
