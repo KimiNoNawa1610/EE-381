1c39a5d7e52edcddfa48ba2141e523589bf61f7a
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-08-31 11:34:41 -0700

    Update Project 1.py

--- Project 1.py
+++ Project 1.py
@@ -5,14 +5,29 @@
 #and it is used to do sumple simulation.
 
 def main():
-    N=10000#Cycle length or norm
-    A=4857#Adder
-    M=8601#Multiplier
 
-    S= int(input("Enter a whole number"))
-    for i in range(100):
-        S=(M*S+A)%N
-        r=S/N#The number r will be in [0,1)
-        print(format(r,'.4f'))
+    def RNG():
+
+        r=[]#Empty list for our random numbers
+        
+        N=10000#Cycle length or norm
+        A=4857#Adder
+        M=8601#Multiplier
+
+        S= int(input("Enter a whole number"))
+        for i in range(100):
+            S=(M*S+A)%N
+            v=S/N#The number r will be in [0,1)
+            r.append(v)
+        return r
+
+    def die(r):
+        import math
+        print("Die roll below")
+        for k in range(10):
+            d=math.floor(6*r[k]+1)
+            print(d)
+
+    s=RNG()
 
 main()
