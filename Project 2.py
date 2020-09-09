dec39270f1cc74108802771d72b518803ac0fbe8
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-09-09 11:54:32 -0700

    Create Project 2.py

---
+++ Project 2.py
@@ -0,0 +1,9 @@
+#----------------
+#EE 381 fall 2020
+#Project 2
+#Nhan Vo
+#I.D. #017771388
+#Start Date: 9-9-2020
+#End Date:
+#Simulation of a coin flip. Use simulation to determine probability of
+#heads on an odd flip.
